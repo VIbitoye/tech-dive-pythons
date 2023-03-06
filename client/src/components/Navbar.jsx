e7f630286c47606ea42b50f9e774945e23ea7432
VIbitoye <112020449+VIbitoye@users.noreply.github.com>
2023-03-06 12:28:32 -0500

    Updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,21 +28,22 @@ function Navbar() {
             ADMINISTRATION
           </Link>
         </div>
-        <div className="sm:hidden">
-        <button className="mobile-menu-button" onClick={toggleMobileMenu}>
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div>
-      <div className={`sm:hidden ease-in-out mobile-menu mt-10 ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
-        <Link to="/exams" className="block px-2 py-2 text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md">
-          EXAMS
-        </Link>
-        <Link to="/admin" className="block px-2 py-2 mt-1 text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md">
-          ADMINISTRATION
-        </Link>
-      </div>
+        <div className="sm:hidden relative">
+  <button className="mobile-menu-button mt-2 right-0" onClick={toggleMobileMenu}>
+    <svg className="w-8 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  </button>
+  <div className={`sm:hidden ease-in-out mobile-menu mt-2 ${isMobileMenuOpen ? 'block' : 'hidden'} bg-[#50936d] ml-2 py-1 px-1 h-fit rounded absolute -right-4 animated slide-in-left`}>
+
+    <Link to="/exams" className="block px-2 py-1 tracking-widest text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md">
+      EXAMS
+    </Link>
+    <Link to="/admin" className="block px-2 py-1 mt-1 text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md">
+      ADMINISTRATION
+    </Link>
+  </div>
+</div>
     </div>
     </div>
   )
