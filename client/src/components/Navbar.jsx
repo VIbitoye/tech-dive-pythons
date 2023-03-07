88d08591de5d170f1523a43c9a6e73955a8a1b9f
VIbitoye <112020449+VIbitoye@users.noreply.github.com>
2023-03-07 15:52:03 -0500

    Updated dropdown menu for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./medical_logo.png";
 import { useState } from "react";
+import './Navbar.css';
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -23,50 +24,30 @@ function Navbar() {
             </span>
           </Link>
         </div>
-        <div className="hidden sm:flex sm:items-center font-semibold sm:ml-6">
-          <Link to="/exams" className="flex items-center px-3 py-2 rounded-md text-white hover:bg-[#266140] bg-[#2e7c4d]">
+        <div className="hidden sm:flex sm:items-center font-semibold sm:ml-6 gap-1">
+          <Link to="/exams" className="bg-[#2e7c4d] text-white active:bg-green-600 font-bold uppercase text-base px-6 py-3 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
             EXAMS
           </Link>
-          <Link to="/admin" className="flex items-center px-3 py-2 ml-4 text-white bg-[#2e7c4d] hover:bg-[#266140] rounded-md">
+          <Link to="/admin" className="bg-[#2e7c4d] text-white active:bg-green-600 font-bold uppercase text-base px-6 py-3 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
             ADMINISTRATION
           </Link>
         </div>
-        <div className="sm:hidden">
-          <button className="mobile-menu-button" onClick={toggleMobileMenu}>
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={`sm:hidden ease-in-out mobile-menu mt-10 ${
-            isMobileMenuOpen ? "block" : "hidden"
-          }`}
-        >
-          <Link
-            to="/exams"
-            className="block px-2 py-2 text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md"
-          >
-            EXAMS
-          </Link>
-          <Link
-            to="/admin"
-            className="block px-2 py-2 mt-1 text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] rounded-md"
-          >
-            ADMINISTRATION
-          </Link>
-        </div>
+        <div className="sm:hidden relative">
+  <button className="mobile-menu-button mt-2 right-0" onClick={toggleMobileMenu}>
+    <svg className="w-8 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  </button>
+  <div className={`sm:hidden ease-in-out mobile-menu mt-2 ${isMobileMenuOpen ? 'block dropdown' : 'hidden'} bg-[#50936d] ml-2 py-1 px-1 h-fit rounded-md absolute -right-4 animated slide-in-left`}>
+
+    <Link to="/exams" className="block px-2 py-1 tracking-wider text-left text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] ">
+      EXAMS
+    </Link>
+    <Link to="/admin" className="block px-2 py-1 mt-1 text-left text-white font-semibold bg-[#2e7c4d] hover:bg-[#266140] ">
+      ADMINISTRATION
+    </Link>
+  </div>
+</div>
       </div>
     </div>
   );
